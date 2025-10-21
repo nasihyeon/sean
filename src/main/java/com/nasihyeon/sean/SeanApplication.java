5cd3ef52f2fa9b7db8754fe7080427caf8be08ab
nasihyeon <nasihyun77@gmail.com>
2025-10-22 00:10:01 +0900

    Initial Spring Boot project

---
+++ src/main/java/com/nasihyeon/sean/SeanApplication.java
@@ -0,0 +1,13 @@
+package com.nasihyeon.sean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeanApplication.class, args);
+	}
+
+}
